c78bd0698ae0934238d08b64b8889b146897d0f1
devyhan93 <devyhan93@gmail.com>
2021-01-08 11:16:32 +0900

    프로젝트 생성

---
+++ GitHubSearch/GitHubSearch/GitHubSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSearchApp.swift
+//  GitHubSearch
+//
+//  Created by 요한 on 2021/01/08.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
