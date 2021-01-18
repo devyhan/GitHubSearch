a22b3bd36c30fdc1dd3aa2a10fb3d8a0774bf077
devyhan93 <devyhan93@gmail.com>
2021-01-18 18:31:23 +0900

    리스트 Section추가

--- GitHubSearch/GitHubSearch/ContentView.swift
+++ GitHubSearch/GitHubSearch/ContentView.swift
@@ -7,16 +7,57 @@
 
 import SwiftUI
 
+struct GroupHeader: View {
+  var backgroundColor: Color
+  var forgroundColor: Color
+  
+  let title: String
+  
+  init(title: String, backgroundColor: Color = Color.white, forgroundColor: Color = Color.black) {
+    self.backgroundColor = backgroundColor
+    self.forgroundColor = forgroundColor
+    self.title = title
+  }
+  
+  var body: some View {
+    HStack {
+      Text(self.title)
+        .font(.headline)
+        .foregroundColor(self.forgroundColor)
+        .padding()
+      
+      Spacer()
+    }
+    .background(self.backgroundColor)
+    .listRowInsets(EdgeInsets(
+                    top: 0,
+                    leading: 0,
+                    bottom: 0,
+                    trailing: 0)
+    )
+  }
+}
+
 struct ContentView: View {
   var body: some View {
     NavigationView {
       List {
-        NavigationLink(
-          destination: ButtonView(),
-          label: {
-            Text("Button")
-          })
-      }.navigationBarTitle("SwiftUI")
+        Section(header: GroupHeader(title: "Component")) {
+          NavigationLink(
+            destination: ButtonView(),
+            label: {
+              Text("Button")
+            })
+        }.listStyle(GroupedListStyle())
+        Section(header: GroupHeader(title: "feature")) {
+          NavigationLink(
+            destination: ButtonView(),
+            label: {
+              Text("Login")
+            })
+        }.listStyle(GroupedListStyle())
+      }
+      .navigationBarTitle("SwiftUI")
     }
   }
 }
