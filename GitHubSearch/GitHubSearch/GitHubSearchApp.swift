2bb2eea5c9ba0c348b6d9d819d468bccf15a0077
devyhan93 <devyhan93@gmail.com>
2021-01-18 12:00:44 +0900

    사용자 국가정보 로깅

--- GitHubSearch/GitHubSearch/GitHubSearchApp.swift
+++ GitHubSearch/GitHubSearch/GitHubSearchApp.swift
@@ -5,13 +5,30 @@
 //  Created by 요한 on 2021/01/18.
 //
 
+import os
 import SwiftUI
+import MapKit
 
 @main
 struct GitHubSearchApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  private let osLog = OSLog(subsystem: "user locale info", category: "GitHubSearch")
+  
+  init() {
+    let locale = Locale.current
+    
+    switch locale.regionCode ?? "empty" {
+    case "EN":
+      os_log("User Locate %@", log: self.osLog, type: .info, "\"EN\"")
+    case "KR":
+      os_log("User Locate %@", log: self.osLog, type: .info, "\"KR\"")
+    default:
+      os_log("User Locate %@", log: self.osLog, type: .info, "\"Noting user locate info\"")
     }
+  }
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
 }
