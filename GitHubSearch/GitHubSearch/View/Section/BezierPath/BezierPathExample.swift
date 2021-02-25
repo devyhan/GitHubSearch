126044caed740d740bd6fde9c885d053e9f1c9cd
devyhan <devyhan93@gmail.com>
2021-02-25 12:39:06 +0900

    하드코딩된 뷰 그리드뷰로 교체

--- GitHubSearch/GitHubSearch/View/Section/BezierPath/BezierPathExample.swift
+++ GitHubSearch/GitHubSearch/View/Section/BezierPath/BezierPathExample.swift
@@ -8,38 +8,78 @@
 import SwiftUI
 
 struct BezierPathExample: View {
+    
+    private let window = UIScreen.main.bounds
+    
     var body: some View {
         VStack {
-            HStack {
-                BezierPathShape(lengthValue: 130, image: "cafe-1")
-                BezierPathShape(lengthValue: 130, image: "cafe-2")
-                BezierPathShape(lengthValue: 130, image: "cafe-3")
-            }
+            Text("BezierPathGrid")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 30)
             
-            HStack {
-                BezierPathShape(lengthValue: 130, image: "cafe-4")
-                BezierPathShape(lengthValue: 130, image: "cafe-5")
-                BezierPathShape(lengthValue: 130, image: "cafe-6")
-            }
+            Text("HGrid")
+                .font(.title)
+                .frame(width: window.width, alignment: .leading)
+                .padding(.leading, 30)
+            HGridView()
+                .frame(height: 130)
             
-            HStack {
-                BezierPathShape(lengthValue: 130, image: "cafe-7")
-                BezierPathShape(lengthValue: 130, image: "cafe-8")
-                BezierPathShape(lengthValue: 130, image: "cafe-9")
-            }
-            
-            HStack {
-                BezierPathShape(lengthValue: 130, image: "cafe-10")
-                BezierPathShape(lengthValue: 130, image: "cafe-11")
-                BezierPathShape(lengthValue: 130, image: "cafe-12")
-            }
-            
-            HStack {
-                BezierPathShape(lengthValue: 130, image: "cafe-13")
-                BezierPathShape(lengthValue: 130, image: "cafe-14")
-                BezierPathShape(lengthValue: 130, image: "cafe-15")
-            }
-        
+            Text("VGrid")
+                .font(.title)
+                .frame(width: window.width, alignment: .leading)
+                .padding(.leading, 30)
+            VGridView()
+                .frame(height: window.height - 400)
+        }
+    }
+}
+
+struct HGridView: View {
+    let items = 1...18
+
+    let rows = [
+        GridItem(.fixed(130)),
+    ]
+
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHGrid(rows: rows, alignment: .center, spacing: 20) {
+                ForEach(items, id: \.self) { item in
+                    VStack {
+                        BezierPathShape(lengthValue: 100, image: "cafe-\(item)")
+                            .padding(.trailing, 110)
+                        Text("cafe\(item)")
+                    }
+                }
+            }.padding(.leading, 30)
+        }
+    }
+}
+
+struct VGridView: View {
+    let items = 1...20
+
+    let columns = [
+        GridItem(.fixed(130)),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(items, id: \.self) { item in
+                    VStack {
+                        BezierPathShape(lengthValue: 100, image: "coffee-\(item)")
+                            .padding(.bottom, 110)
+                            
+                        Text("coffee\(item)")
+                            .padding(.trailing, 30)
+                            
+                    }
+                }
+            }.padding(.leading, 30)
         }
     }
 }
