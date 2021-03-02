71baa18bb20c4e21e1d7565a5cdbf46c5d0148ae
devyhan <devyhan93@gmail.com>
2021-03-02 12:11:59 +0900

    네비게이션 구조 정리 및 ColorSet 뷰생성

---
+++ GitHubSearch/GitHubSearch/View/Section/ColorSet/ColorSet.swift
@@ -0,0 +1,20 @@
+//
+//  ColorSet.swift
+//  GitHubSearch
+//
+//  Created by 조요한 on 2021/03/02.
+//
+
+import SwiftUI
+
+struct ColorSet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorSet_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorSet()
+    }
+}
